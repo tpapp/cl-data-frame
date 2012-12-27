520c4a71dd3b8b227a493fbe09257a7c63ec0fff
tpapp <tkpapp@gmail.com>
2012-12-27 17:11:35 +0100

    Renamed test suite, added slice tests.

--- cl-data-frame-tests.lisp
+++ cl-data-frame-tests.lisp
@@ -17,15 +17,15 @@
 (defun run (&optional interactive?)
   (run-suite 'data-frame :use-debugger interactive?))
 
-(defsuite data-frame-creation (data-frame))
+(defsuite data-frame-basics (data-frame))
 
-(deffixture data-frame-creation (@body)
+(deffixture data-frame-basics (@body)
   (let ((v #(1 2 3))
         (b #*0110)
         (s #(a b c d)))
     @body))
 
-(deftest data-frame-creation (data-frame-creation)
+(deftest data-frame-creation (data-frame-basics)
   (let ((df (data-frame :vector v :symbols s)))
     (assert-equalp '(:vector :symbols)
         (column-names df))
@@ -35,3 +35,11 @@
         (columns-plist df))
     (assert-equalp `((:vector . ,v) (:symbols . ,s))
         (columns-alist df))))
+
+(deftest data-frame-slice (data-frame-basics)
+  (let ((df (data-frame :vector v :symbols s)))
+    (assert-equalp `(:vector ,v)
+        (columns-plist (slice df t #(:vector))))
+    (assert-equalp `(:vector ,(slice v b))
+        (columns-plist (slice df b #(0))))
+    ))
